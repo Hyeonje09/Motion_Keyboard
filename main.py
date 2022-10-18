fb23bc9412540e859390d219cc9163223a00f73d
hyeonje <jhj37299455@gmail.com>
2022-10-18 13:45:15 +0900

    keyboard display change

--- main.py
+++ main.py
@@ -13,29 +13,35 @@ keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B","N", "M", ",", ".", "/"]]
 
+def drawAll(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(cam_img, button.pos, (x + w, y + h), (255,0,255),cv2.FILLED)
+        cv2.putText(cam_img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4) 
+
+    return img
+
 class button():
     def __init__(self, pos, text, size=[85, 85]):
         self.pos = pos
         self.size = size
         self.text = text
 
-        x, y = self.pos
-        w, h = self.size
-        cv2.rectangle(cam_img, self.pos, (x + w, y + h), (255,0,255),cv2.FILLED)
-        cv2.putText(cam_img, self.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4) 
-
 buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(button([100 * j + 50, 100 * i + 50], key))
 
 while True:
     sucess, cam_img = cap_cam.read()
     cam_img = detector.findHands(cam_img)
     lm_list, bboxInfo = detector.findPosition(cam_img)
-
     cam_img = cv2.flip(cam_img, 1)
+    cam_img = drawAll(cam_img, buttonList)
+    
  
-    for i in range(len(keys)):
-        for j, key in enumerate(keys[i]):
-            buttonList.append(button([100 * j + 50, 100 * i + 50], key))
+   
     
     cv2.imshow("image", cam_img)
     if cv2.waitKey(1) == ord('q'): 
